8e888dec7163a25b94f3a238bcc8fe2fd2837472
Toni Cebrián <toni@tonicebrian.com>
2024-11-25 11:04:31 +0100

    Set imports

--- test/resources/golden/SimplePetStore.purs
+++ test/resources/golden/SimplePetStore.purs
@@ -1,3 +1,11 @@
 module SimplePetStore where
 
 import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+import Affjax (get, Response)
+import Affjax.RequestHeader (RequestHeader)
+import Data.Argonaut
+import Data.Either (Either(..))
+import Effect.Aff (launchAff)
+import Affjax.ResponseFormat
